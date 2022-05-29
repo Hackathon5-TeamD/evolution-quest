4f22f883fcd22e57b583d2ea2c35522fbf1eee7d
Junichi Nishi <github@purewest.dip.jp>
2022-05-29 22:31:02 +0900

    Flaskコンテナの修正

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -9,10 +9,12 @@ done
 # cd /usr/src/app/db && alembic upgrade head
 # cd /usr/src/app/db && python seed.py
 
-apt install -y locales
+# apt install -y locales
 # cd /usr/src/app/app/sql_app && python database.py
-echo "ja_JP UTF-8" > /etc/locale.gen && locale-gen
+# echo "ja_JP UTF-8" > /etc/locale.gen && locale-gen
 
 # cd /usr/src/app/app && uvicorn sql_app.main:app --reload --port=8000 --host=0.0.0.0
 # cd /usr/src/app/app && gunicorn sql_app.main:app -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000
+# cd /usr/src/app && gunicorn app.app:app -w 4 -k 0.0.0.0:8000
 
+cd /usr/src/app && flask run --host=0.0.0.0
