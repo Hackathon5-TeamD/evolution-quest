b9b9e16aba08010224bba7798258fbf4fa646433
kobakichi <fenderstrat57vin@gmail.com>
2022-06-02 05:41:08 +0900

    componentsディレクトリ下にpagesディレクトリを作成。各ページのディレクトリと、Register.tsxだけ作成。/registerでRegister.tsxを読み込むようRouter.tsxに記載。”

--- front/React/src/router/Router.tsx
+++ front/React/src/router/Router.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../components/Login";
+import { Register } from "../components/pages/Register/Register";
 
 export const Router = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </>
   );
