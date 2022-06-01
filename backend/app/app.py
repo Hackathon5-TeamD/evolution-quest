54baacdef8eb69c32bbffc7a851d44e54fb69bb8
Terralien-jp <terralienjp@gmail.com>
2022-06-02 01:06:41 +0900

    分割完了

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,21 @@
 from flask import Flask
 
+# モジュールのインポート
+from user import user_module
+from terminologie import terminologie_module
+from result import result_module
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello,World"
+
+# コントローラー先
+app.register_blueprint(user_module)
+app.register_blueprint(terminologie_module)
+app.register_blueprint(result_module)
+
 
 if __name__ == '__main__':
     app.run()
