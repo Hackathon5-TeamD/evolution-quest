2ffdfad5d0152561e209cc7441e935579e45b3d9
haru-24 <h24wler@gmail.com>
2022-06-03 00:24:32 +0900

    Add data to sqlite at docker startup

--- backend/app/insert_terminologies.py
+++ backend/app/insert_terminologies.py
@@ -10,7 +10,7 @@ def insert_terminologie():
     """
     問題のデータをコンテナ立ち上げ時に挿入する.
     """
-    with open("app/csv/terminologie.csv", "r") as f:
+    with open("csv/terminologie.csv", "r") as f:
         data = csv.reader(f)
         for i, row in enumerate(data):
             if i == 0:
--- backend/app/model.py
+++ backend/app/model.py
@@ -1,8 +1,4 @@
 import os
-import sys
-
-sys.path.append("/Users/nishiuraharu/.pyenv/versions/3.9.7/lib/python3.9/site-packages")
-
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
