5f84ff6f23d309a4c7e43b777be663748359598e
kobakichi <fenderstrat57vin@gmail.com>
2022-06-02 23:25:00 +0900

    各ページの作成。Router.tsxへの読み込み完了。

--- front/React/src/router/Router.tsx
+++ front/React/src/router/Router.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../components/Login";
+import { MainPage } from "../components/pages/Main/MainPage";
 import { Register } from "../components/pages/Register/Register";
+import { ResultRank } from "../components/pages/Result/ResultRank";
+import { GameStart } from "../components/pages/Start/GameStart";
+import { UserPage } from "../components/pages/User/UserPage";
 
 export const Router = () => {
   return (
@@ -9,6 +13,10 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="gamestart" element={<GameStart />} />
+        <Route path="userpage" element={<UserPage />} />
+        <Route path="main" element={<MainPage />} />
+        <Route path="result" element={<ResultRank />} />
       </Routes>
     </>
   );
