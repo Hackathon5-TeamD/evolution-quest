677cb6dc92612bdf1505682288b611b016c4ae6a
daisuke imamura <d-imamura@purewest.dip.jp>
2022-06-03 15:37:27 +0900

    flask雛形完了。

--- backend/app/flaskr/views.py
+++ backend/app/flaskr/views.py
@@ -1,7 +1,8 @@
+# import re
 from flask import Blueprint, request, render_template, redirect, url_for
 from flask_login import login_user, login_required, logout_user
-from backend.app.forms import LoginForm, RegisterForm
-from backend.app.model import User
+from flaskr.forms import LoginForm, RegisterForm
+from flaskr.models import User
 
 bp = Blueprint('app', __name__, url_prefix='')
 
@@ -9,43 +10,45 @@ bp = Blueprint('app', __name__, url_prefix='')
 def home():
     return render_template('home.html')
 
-#ログインしてないと実行されない(login_userが実行されていない)
+#ログインしていないと実行されない（login_userが実行されていないと）
 #ログインしていない場合はlogin関数に飛ばされる
-@bp.route('/welcome')
+@bp.route(('/welcome'))
 @login_required
 def welcome():
     return render_template('welcome.html')
 
 @bp.route('/logout')
 @login_required
-def logout_user():
-    logout_user()
+def logout():
+    logout_user()  #ログアウトできる
     return redirect(url_for('app.home'))
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
-    if request.methods == 'POST' and form.validate():
-        user = User.select_by_username(form.username.data)
+    if request.method == 'POST' and form.validate():
+        user = User.select_by_email(form.email.data)
+        #emailから取得したUserのパスワードとクライアントが入力したパスワードが一致
         if user and user.validate_password(form.password.data):
-            login_user(user)
-            next = request.args.get('next')
+            login_user(user, remember=True)
+            next = request.args.get('next')  # 次のURL
             if not next:
-                netx = url_for('app.welcome')
+                next = url_for('app.welcome')
             return redirect(next)
-    return render_template('login.html', form = form)
-
+    return render_template('login.html', form=form)
+    
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
-    if request.methods == 'POST' and form.validate():
+    if request.method == 'POST' and form.validate():
         user = User(
+            email = form.email.data,
             username = form.username.data,
             password = form.password.data
         )
         user.add_user()
         return redirect(url_for('app.login'))
-    return render_template('register.html', form =form)
+    return render_template('register.html', form=form)
 
 @bp.route('/user')
 @login_required
