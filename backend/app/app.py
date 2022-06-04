424d33c9d060086ce23298d914d8d8dd7d0abda3
Terralien-jp <terralienjp@gmail.com>
2022-06-05 03:16:11 +0900

    Flaskコンテナが起動してれば動作する

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,20 @@
 from flask import Flask
 
+# モジュールのインポート
+from user import user_module
+from terminologie import terminologie_module
+from result import result_module
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return "Hello,World"
 
-if __name__ == '__main__':
-    app.run()
+# コントローラー先
+app.register_blueprint(user_module)
+app.register_blueprint(terminologie_module)
+app.register_blueprint(result_module)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ backend/app/user.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+user_module = Blueprint("person", __name__)
+
+@user_module.route("/user/", methods=["GET"])
+def user():
+    return "user test" 
