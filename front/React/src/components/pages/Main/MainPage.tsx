16b39dcb06406dec0b7abc8e5940da22c4610f17
Rinasham <rinashamau@gmail.com>
2022-06-06 23:56:30 +1000

    [add] main page design

--- front/React/src/components/pages/Main/MainPage.tsx
+++ front/React/src/components/pages/Main/MainPage.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+import styles from "./MainPage.module.css";
+import "react-simple-keyboard/build/css/index.css";
+import { GameArea } from "./gameArea/GameArea";
+import { RecordArea } from "./recordArea/RecordArea";
 
 export const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <div className={styles.gamePageWrapper}>
+      <GameArea />
+      <RecordArea />
+    </div>
+  );
 };
