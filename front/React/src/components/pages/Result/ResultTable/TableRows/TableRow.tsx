309d4b9488db6fd1b47580db2006be728dfc9f19
Rinasham <rinashamau@gmail.com>
2022-06-09 01:44:16 +1000

    [add] table row CSS

--- front/React/src/components/pages/Result/ResultTable/TableRows/TableRow.tsx
+++ front/React/src/components/pages/Result/ResultTable/TableRows/TableRow.tsx
@@ -1,4 +1,5 @@
 import { Table } from "semantic-ui-react";
+import styles from "./TableRow.module.css";
 
 type Props = {
   idx: number;
@@ -15,15 +16,15 @@ export const TableRow = (props: Props) => {
   return (
     <Table.Row>
       {currentPage === 1 ? (
-        <Table.Cell>{idx + 1}</Table.Cell>
+        <Table.Cell className={styles.cell}>{idx + 1}</Table.Cell>
       ) : (
-        <Table.Cell>{idx + 11}</Table.Cell>
+        <Table.Cell className={styles.cell}>{idx + 11}</Table.Cell>
       )}
 
-      <Table.Cell>{user_name}</Table.Cell>
-      <Table.Cell>{played_at}</Table.Cell>
-      <Table.Cell>{accuracy} %</Table.Cell>
-      <Table.Cell>{wpm} WPM</Table.Cell>
+      <Table.Cell className={styles.cell}>{user_name}</Table.Cell>
+      <Table.Cell className={styles.cell}>{played_at}</Table.Cell>
+      <Table.Cell className={styles.cell}>{accuracy} %</Table.Cell>
+      <Table.Cell className={styles.cell}>{wpm} WPM</Table.Cell>
     </Table.Row>
   );
 };
