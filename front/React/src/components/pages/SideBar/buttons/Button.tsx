15200986c486c0ec84d7e231692c91c483704cdd
Rinasham <rinashamau@gmail.com>
2022-06-09 22:08:44 +1000

    [fix] sidebarリンクnavigation

---
+++ front/React/src/components/pages/SideBar/buttons/Button.tsx
@@ -0,0 +1,23 @@
+import styles from "./Button.module.css";
+import "../SideBar.css";
+import { useNavigate } from "react-router-dom";
+import { FC, memo, ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  icon: JSX.Element;
+  linkTo: string;
+};
+
+export const Button: FC<Props> = memo((props) => {
+  const { children, icon, linkTo } = props;
+  const navigation = useNavigate();
+  console.log("ボタン再レンダリング");
+
+  return (
+    <button className={styles.menuItem} onClick={() => navigation(linkTo)}>
+      {icon}
+      {children}
+    </button>
+  );
+});
