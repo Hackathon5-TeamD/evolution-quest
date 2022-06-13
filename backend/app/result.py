c2e94f0362c235ca0da5e133cf86f05a9d2f8d7f
daisuke imamura <d-imamura@purewest.dip.jp>
2022-06-13 14:36:40 +0900

    userのgetを検証

--- backend/app/result.py
+++ backend/app/result.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 result_module = Blueprint("result", __name__)
 
-@result_module.route("/result/", methods=["GET"])
+@result_module.route("/result/")
 def result():
     return "result test" 
--- backend/app/user.py
+++ backend/app/user.py
@@ -1,7 +1,21 @@
 from flask import Blueprint
+from model import Person, db, app
 
 user_module = Blueprint("user_module", __name__)
 
-@user_module.route("/user/")
-def user():
-    return "user test" 
+# @user_module.route("/user/")
+# def user():
+#     return "user test" 
+@app.route('/user')
+def get_user():
+    
+    payload = request.json
+    insert_data = Person(
+        id = payload.get("id"),
+        user_name = payload.get("user_name"),
+        password = payload.get("password")
+    )
+    db.session.add(insert_data)
+    db.session.commit()
+    return payload
+   
