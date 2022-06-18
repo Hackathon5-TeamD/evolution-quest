ddb951ae00939299e9452709bb4fb8ff6d76b0b4
kobakichi <fenderstrat57vin@gmail.com>
2022-06-19 03:02:17 +0900

    postGameDataの引数にuser_idを追加して、dataにもuser_idを追加

--- front/React/src/api/postGameData.ts
+++ front/React/src/api/postGameData.ts
@@ -2,14 +2,16 @@ import axios from "axios";
 const Url = `http://localhost:3001`;
 
 export const postGameData = async (result: {
+  user_id: number;
   accuracy: number;
   wpm: number;
 }) => {
-  const { accuracy, wpm } = result;
+  const { accuracy, wpm, user_id } = result;
   const data = {
-    "accuracy_value": accuracy,
-    "wpm": wpm,
-    "played_at": Date.now(),
+    user_id: user_id,
+    accuracy_value: accuracy,
+    wpm: wpm,
+    played_at: Date.now(),
   };
   try {
     const res = await axios.post(`${Url}/result`, data);
