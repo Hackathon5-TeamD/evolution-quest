5243671841a7bea3ad3095572cc463dd54b81bf0
Junichi Nishi <github@purewest.dip.jp>
2022-06-21 21:58:50 +0900

    NodeコンテナとNginxのコンテナの統合テスト

--- cache-clear.sh
+++ cache-clear.sh
@@ -5,8 +5,12 @@ rm -rf backend/flask_app/app/data/__pycache__
 rm -rf backend/flask_app/app/venv
 rm -rf backend/flask_app/app/data.sqlite
 rm -rf backend/flask_app/app/migrations
+rm -rf backend/flask_app/app/scripts
 
-# rm -rf front/React/node_modules
-# rm -rf front/React/build
+rm -rf front/React/node_modules
+rm -rf front/React/build
+
+rm -rf phpmyadmin
+rm -rf gunicorn_socket
 
 rm -rf log
