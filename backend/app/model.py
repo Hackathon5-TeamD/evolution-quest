0e2a0a92ea6de14f4e937960bef078492228b194
Terralien-jp <terralienjp@gmail.com>
2022-06-26 12:17:49 +0900

    fix 不要な部分削除、不要と思われる行コメントアウト、in:pymysql out:flask-login

--- backend/app/model.py
+++ backend/app/model.py
@@ -1,11 +1,8 @@
-from audioop import cross
+# from audioop import cross
 import os
-from unittest import result
+# from unittest import result
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-# 使用していないと思われるため一旦コメントアウト
-# import pytz
 
 from flask_migrate import Migrate
 from flask_login import UserMixin, LoginManager
@@ -26,16 +23,14 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://{user}:{password}@{host
       'port': os.getenv("MYSQL_PORT"),
       'database': os.getenv('MYSQL_DATABASE'),
     })
-  
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
-#     base_dir, "data.sqlite"
-# )
+
 #jwtとの関係があるのかな？いらない？
-#session情報の暗号化？
-app.config['SECRET_KEY'] = os.urandom(24)
+#session情報の暗号化？現状不使用
+# app.config['SECRET_KEY'] = os.urandom(24)
 
 # 使用しない機能と思うため,また明示的にオフしておかないとエラーが出ることがある
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
 Migrate(app, db)
 
@@ -75,7 +70,3 @@ class Result(db.Model):
     accuracy_value = db.Column(db.Float)
     wpm = db.Column(db.Float)
     playd_at_date = db.Column(db.Integer)
-    
-    
-    # def result_user():
-    #     return SELECT * FROM users CROSS JOIN results;
